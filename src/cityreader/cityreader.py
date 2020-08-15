1e94b36b6aa124034a188a277d6f136235d2ad82
Kim Ly Buck <kim.3993@gmail.com>
2020-08-14 19:39:59 -0500

    mvp done

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,15 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
+import csv
+class City():
+    def __init__(self, name, lat, lon):
+        self.name = name
+        self.lat = float(lat)
+        self.lon = float(lon)
+
+    def __str__(self):
+        return f'{self.name}, {self.lat}, {self.lon}'
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -17,11 +26,16 @@
 cities = []
 
 def cityreader(cities=[]):
-  # TODO Implement the functionality to read from the 'cities.csv' file
-  # Ensure that the lat and lon valuse are all floats
-  # For each city record, create a new City instance and add it to the 
-  # `cities` list
-    
+# TODO Implement the functionality to read from the 'cities.csv' file
+# Ensure that the lat and lon valuse are all floats
+# For each city record, create a new City instance and add it to the 
+# `cities` list
+    with open('./cities.csv') as cities_file:
+        reader = csv.DictReader(cities_file)
+        # city,  state_name,  county_name,    lat,    lng,    population,     density,   timezone,    zips
+        for row in reader:
+            city = City(row['city'], row['lat'], row['lng'])
+            cities.append(city)
     return cities
 
 cityreader(cities)
@@ -62,10 +76,10 @@ for c in cities:
 # TODO Get latitude and longitude values from the user
 
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
-  # within will hold the cities that fall within the specified region
-  within = []
-  
-  # Go through each city and check to see if it falls within 
-  # the specified coordinates.
+# within will hold the cities that fall within the specified region
+    within = []
 
-  return within
+# Go through each city and check to see if it falls within 
+# the specified coordinates.
+
+    return within
